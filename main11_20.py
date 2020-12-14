3f3c56ae2b89e9917a5a36e4802f1846da8d140e
drafteee <sergej_-96@mail.ru>
2020-12-14 22:53:54 +0300

    finished lesson12

--- main11_20.py
+++ main11_20.py
@@ -1,11 +1,12 @@
 import pdb
 
-def startFile():
-  print('--------------------------')
 
-  def add(num1, num2):
-    pdb.set_trace()
+def add(num1, num2):
+    # pdb.set_trace()
     return num1 + num2
-  
-  
-  add(1,2)
+
+
+add(1, 2)
+
+with open('main.py', mode='r') as my_file:  # r w r+w a
+    print(my_file.readline())
