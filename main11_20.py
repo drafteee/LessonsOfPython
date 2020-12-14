7d40264a84790db0b1087bc7519d44fcef22033b
drafteee <sergej_-96@mail.ru>
2020-12-14 23:12:40 +0300

    finished lesson13

--- main11_20.py
+++ main11_20.py
@@ -1,4 +1,5 @@
 import pdb
+import re
 
 
 def add(num1, num2):
@@ -10,3 +11,10 @@ add(1, 2)
 
 with open('main.py', mode='r') as my_file:  # r w r+w a
     print(my_file.readline())
+
+pattern = re.compile('this')
+string = 'search inside of this this text plesase!'
+
+print(pattern.search(string))
+print(pattern.findall(string))
+print(pattern.match(string))
