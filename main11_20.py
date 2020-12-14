a04bedc480687a183eee94e048995b363e3323a3
drafteee <sergej_-96@mail.ru>
2020-12-14 22:33:52 +0300

    up

--- main11_20.py
+++ main11_20.py
@@ -1,10 +1,9 @@
 import pdb
 
-def startFile():
-  print('--------------------------')
 
-  def add(num1, num2):
-    pdb.set_trace()
+def add(num1, num2):
+    # pdb.set_trace()
     return num1 + num2
-  
-  add(1,2)
+
+
+add(1, 2)
